026ef203b5f39d3112aa5d1cab2c65cbc357bca5
Ponmana selvan <selvasa2277@gmail.com>
2022-10-18 11:01:57 +0545

    first

---
+++ register/application/controllers/Register.php
@@ -0,0 +1,10 @@
+<?php 
+
+    class Register extends CI_Controller{
+
+        public function registerpage(){
+            $this->load->view('regist');
+        }
+    }
+
+?>
---
+++ register/application/views/regist.php
@@ -0,0 +1,2 @@
+<link rel='stylesheet' href='<?php print base_url('assets/css/register.css');?>'/>
+<h1>this is register form.php and </h1>
